0ba9bea8fdcc5587d6903a7aa40f6aa10b0d9c5d
ermanzi <ermanzi@mit.edu>
2017-04-30 18:40:54 -0400

    initial commit

---
+++ lqr-control-kosta.py
@@ -0,0 +1,59 @@
+from __future__ import division, print_function
+ 
+import numpy as np
+import scipy.linalg
+ 
+def lqr(A,B,Q,R):
+    """Solve the continuous time lqr controller.
+     
+    dx/dt = A x + B u
+     
+    cost = integral x.T*Q*x + u.T*R*u
+    """
+    #ref Bertsekas, p.151
+ 
+    #first, try to solve the ricatti equation
+    X = np.matrix(scipy.linalg.solve_continuous_are(A, B, Q, R))
+    print(B)
+    print(B.T)
+    #compute the LQR gain
+    K = np.matrix(scipy.linalg.inv(R)*(B.T*X))
+     
+    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
+     
+    return K, X, eigVals
+ 
+def dlqr(A,B,Q,R):
+    """Solve the discrete time lqr controller.
+     
+     
+    x[k+1] = A x[k] + B u[k]
+     
+    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
+    """
+    #ref Bertsekas, p.151
+ 
+    #first, try to solve the ricatti equation
+    X = np.matrix(scipy.linalg.solve_discrete_are(A, B, Q, R))
+     
+    #compute the LQR gain
+    K = np.matrix(scipy.linalg.inv(B.T*X*B+R)*(B.T*X*A))
+     
+    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
+     
+    return K, X, eigVals
+
+A = np.matrix([[0,1],[0,0]])
+B = np.matrix([[0],[1]])
+Q = np.matrix([[1,0],[0,0]])
+R = np.matrix([[1]])
+
+K, X, eigVals = lqr(A,B,Q,R)
+
+print("K:")
+print(K)
+print("X:")
+print(X)
+print("---")
+print(eigVals)
+
